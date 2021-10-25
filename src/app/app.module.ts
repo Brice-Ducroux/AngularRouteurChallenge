8df29c617219c5ff8542d437ecb8d6ea1f77dd8a
Brice-Ducroux <brice-ducroux_student2021@wilder.school>
2021-10-25 09:58:06 +0200

    angular exercice is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfilComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { UserProfilComponent } from './user-profil/user-profil.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+const routes: Routes = [
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'user-profil', component: UserProfilComponent },
+  { path: '', component: UserProfilComponent },
+];
+
+export { routes };
